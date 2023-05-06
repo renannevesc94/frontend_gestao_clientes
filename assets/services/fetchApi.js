a87f3fd41326b046b32e6d963fb7b7c0268634f5
Renan Neves <renan.epd@gmail.com>
2023-05-05 21:00:48 -0300

    refac

--- assets/services/fetchApi.js
+++ assets/services/fetchApi.js
@@ -1,5 +1,5 @@
 function CreateFetch(headers = {}) {
-    const baseRota = 'http://192.168.1.64:3000' //Aqui vem a URL padrão da API
+    const baseRota = 'https://api-gestao-clientes.onrender.com' //Aqui vem a URL padrão da API
     headers = { 'Content-Type': 'application/json' }
 
     function setAuth(token) {
