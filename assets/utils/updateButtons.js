51009050caa6997b17360315ce4c5c85020014f3
Renan Neves <renan.epd@gmail.com>
2023-05-08 21:09:19 -0300

    refactoring 2.1

--- assets/utils/updateButtons.js
+++ assets/utils/updateButtons.js
@@ -1,12 +1,14 @@
 
 function updateButtonForm(typeButton) {
     let btnUpdate = document.getElementById('btn-cadastrar');
+    let btnCancelar = document.getElementById('btnCancelar');
     if (typeButton === 'update') {
       if (btnUpdate) {
         btnUpdate.id = 'btn-update';
         btnUpdate.textContent = 'Atualizar';
         btnUpdate.classList.remove('is-success');
         btnUpdate.classList.add('is-link');
+        btnCancelar.textContent = 'Fechar'
       }
   
     } else {
@@ -16,6 +18,7 @@ function updateButtonForm(typeButton) {
         btnUpdate.textContent = 'Cadastrar';
         btnUpdate.classList.remove('is-link');
         btnUpdate.classList.add('is-success');
+        btnCancelar.textContent = 'Cancelar'
 
         document.querySelector('textarea[name="alerta"]').value = ''
         document.querySelector('input[name="cnpj"]').value = '';
